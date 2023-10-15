da115fbeb533d6de80b3953404814f0bc81b2308
Allset <133010630+DeveloperFlow@users.noreply.github.com>
2023-10-15 22:04:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 (function(){
   var style = document.createElement("style");
-  style.innerHTML = "body *{background-color:#000 !important; color:#fff !important;}"
+  style.innerHTML = "body.yotta-darkmode *{background-color:#000 !important; color:#fff !important;}"
+  document.head.appendChild(style)
+  // load the mod menu
+  function modMenu(){}
 }()) 
