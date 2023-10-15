5a7541a08011b48674c730a1b756002b08cac2a8
Allset <133010630+DeveloperFlow@users.noreply.github.com>
2023-10-15 22:07:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,15 @@
   style.innerHTML = "body.yotta-darkmode *{background-color:#000 !important; color:#fff !important;}"
   document.head.appendChild(style)
   // load the mod menu
-  function modMenu(){}
+  function modMenu(){
+    var modBar = document.createElement("div")
+    modBar.className = "yotta-mod-bar"
+    var modIniBtn = document.createElement("button")
+    modIniBtn.className = "yotta-mod-btn"
+    modBar.appendChild(modIniBtn)
+    modIniBtn.onclick = openModMenu
+    modBar.appendChild(modIniBtn)
+    document.body.appendChild(modBar)
+  }
+  function openModMenu(){}
 }()) 
